7c13b50c93290e79e527e84486f31b20d23bc837
SEAN TAN <sean.tan.2021@scis.smu.edu.sg>
2023-09-23 18:51:36 +0800

    HR frontend

--- frontend/src/views/HRStaffView.vue
+++ frontend/src/views/HRStaffView.vue
@@ -1,12 +1,15 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+// import TheWelcome from '../components/TheWelcome.vue'
 import TopNav from '../components/TopNav.vue'
+import RoleManage from '../components/RoleManage.vue'
 
 </script>
 
 <template>
     <div>
         <TopNav></TopNav>
-        <TheWelcome></TheWelcome>
+        <!-- <TheWelcome></TheWelcome> -->
+        <RoleManage></RoleManage>
+        
     </div>
 </template>
