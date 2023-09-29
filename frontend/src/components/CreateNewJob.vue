24c08b58fc84a704daa3405e5ae2dff36f384967
SEAN TAN <sean.tan.2021@scis.smu.edu.sg>
2023-09-29 16:37:15 +0800

    edit create job list form frontend

---
+++ frontend/src/components/CreateNewJob.vue
@@ -0,0 +1,46 @@
+<template>
+    <div>
+      <h2>Create a New Job Listing</h2>
+  
+      <form @submit.prevent="submitJobListing">
+        <!-- Add form fields for job listing details -->
+        <div class="form-group">
+          <label for="description">Job Description:</label>
+          <input type="text" id="description" v-model="newJob.description" required>
+        </div>
+        
+        <!-- Add more form fields for other job details (responsibilities, qualifications, etc.) -->
+        
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        newJob: {
+          description: '',
+          // Add more properties for other job details
+        },
+      };
+    },
+    methods: {
+      submitJobListing() {
+        // Handle the submission logic here (e.g., send data to the backend)
+        console.log('New job listing data:', this.newJob);
+        // Reset form fields after submission if needed
+        this.newJob = {
+          description: '',
+          // Reset other properties as well
+        };
+      },
+    },
+  };
+  </script>
+  
+  <style scoped>
+  /* Add your component-specific styles here */
+  </style>
+  
--- frontend/src/views/newjoblistView.vue
+++ frontend/src/views/newjoblistView.vue
@@ -1,13 +1,39 @@
+
 <script setup>
 // import TheWelcome from '../components/TheWelcome.vue'
 import TopNav from '../components/TopNav.vue'
-
+import CreateNewJob from '../components/CreateNewJob.vue'
 </script>
 
+
 <template>
-  <div>
-    <TopNav></TopNav>
-    <!-- <TheWelcome></TheWelcome> -->
-    <h1>hi</h1>
-  </div>
-</template>
+    <div>
+        <TopNav></TopNav>
+        <!-- <TheWelcome></TheWelcome> -->
+      
+      <!-- Display the job listing creation form -->
+      <CreateNewJob></CreateNewJob>
+      
+      <!-- Display the job listings table (if needed) -->
+      <!-- <div class="table-container">
+        <table class="job-listing-table"> -->
+          <!-- ... Table contents ... -->
+        <!-- </table>
+      </div> -->
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    components: {
+      CreateNewJob,
+    },
+    
+    // ... Other component code ...
+  };
+  </script>
+  
+  <style scoped>
+  /* Add your component-specific styles here */
+  </style>
+  
