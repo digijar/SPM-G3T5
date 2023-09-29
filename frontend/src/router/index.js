22cb1a9b91c6817b8bdc84f88e43a9d51caf2abd
iywq <ivan.yeowq@gmail.com>
2023-09-30 02:03:18 +0800

    Added Routing to Applications Page
    Path for applications is '/hrstaff/applications', component is HRApplicationsView

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -33,6 +33,11 @@ const router = createRouter({
       name: 'newjoblisting',
       component: newjoblistView
     },
+    {
+      path: '/hrstaff/application',
+      name: 'hrstaffapplications',
+      component: HRApplicationsView
+    }
   ]
 })
 
