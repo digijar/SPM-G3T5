777407aca6f9345edd825dc10c26ab4bb5cb2244
iywq <ivan.yeowq@gmail.com>
2023-09-30 01:58:41 +0800

    Create NavBar for HR Page
    Added a different navbar for HR page, to include applications tab

---
+++ frontend/src/components/TopNavHR.vue
@@ -0,0 +1,30 @@
+<template>
+    <!-- <nav class="navbar bg-body-tertiary fixed-top">
+        <div class="container d-flex justify-content-between">
+            <div>
+                <router-link to="/" class="nav-item">
+                    <p class="nav-link text-dark" style="font-size: 26px; font-weight: 800;">SPM-fiesta</p>
+                </router-link>
+            </div>
+        </div>
+    </nav> -->
+
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="/">SPM-fiesta</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="navbar-nav">
+                <a class="nav-link active" aria-current="page" href="/">Home</a>
+            </div>
+            <div class="navbar-nav">
+                <a class="nav-link active" aria-current="page" href="/applications">Applications</a>
+            </div>
+            </div>
+        </div>
+    </nav>
+
+    <hr class="solid" style="height: 2px; border-top: 2px solid black; margin: 0px; padding: 0px">
+</template>
