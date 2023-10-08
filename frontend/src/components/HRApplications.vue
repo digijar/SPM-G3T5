79c35c28032550bf9674ba37d14c8a7d50deb967
izumiaa <91410858+izumiaa@users.noreply.github.com>
2023-10-08 16:42:21 +0800

    Add filtering of applications by features

--- frontend/src/components/HRApplications.vue
+++ frontend/src/components/HRApplications.vue
@@ -4,10 +4,41 @@
       <h1 class="title">Applications</h1>
     </div>
 
+    <div class="row">
+      <div class="filter-container col-2">
+      <h2>Filters 
+        <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-filter" viewBox="0 0 16 16">
+          <path d="M6 10.5a.5.5 0 0 1 .5-.5h3a.5.5 0 0 1 0 1h-3a.5.5 0 0 1-.5-.5zm-2-3a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5zm-2-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5z"/>
+        </svg>
+      </h2>
+      <div class="filter-section">
+        <h3>Role Names</h3>
+        <div v-for="roleName in uniqueRoleNames" :key="roleName">
+          <input
+            type="checkbox"
+            :value="roleName"
+            v-model="selectedRoleNames"
+          />
+          {{ roleName }}
+        </div>
+      </div>
+      <div class="filter-section">
+        <h3>Departments</h3>
+        <div v-for="dept in uniqueDepts" :key="dept">
+          <input
+            type="checkbox"
+            :value="dept"
+            v-model="selectedDepts"
+          />
+          {{ dept }}
+        </div>
+      </div>
+    </div>
+
     <!-- Table container -->
-    <div class="table-container">
-      <input type="text" v-model="searchQuery" placeholder="Search Application">
-    
+    <div class="table-container col-10">
+      <input type="text" v-model="searchQuery" placeholder="Search Application" />
+
       <table class="job-listing-table">
         <thead>
           <tr>
@@ -16,20 +47,30 @@
             <th class="text-center">Staff Name</th>
             <th class="text-center">Department</th>
             <th class="text-center">Skills Match Percentage</th>
+            <th class="text-center">Action</th>
           </tr>
         </thead>
         <tbody>
           <!-- Loop through applications data and display it -->
-                <tr v-for="(application, index) in applications" :key="application.Application_ID" @click="showPopup(application)">
-                  <td class="text-center">{{ application.Application_ID }}</td>
-                  <td class="text-center">{{ application.Role_Name }}</td>
-                  <td class="text-center">{{ application.Staff_Name }}</td>
-                  <td class="text-center">{{ application.Current_Dept }}</td>
-                  <td class="text-center">{{ application.Skills_Match_Percentage }}%</td>
-                </tr>
-              </tbody>
-            </table>
-          </div>
+          <tr
+            v-for="(application, index) in filteredApplications"
+            :key="application.Application_ID"
+            @click="showPopup(application)"
+          >
+            <td class="text-center">{{ application.Application_ID }}</td>
+            <td class="text-center">{{ application.Role_Name }}</td>
+            <td class="text-center">{{ application.Staff_Name }}</td>
+            <td class="text-center">{{ application.Current_Dept }}</td>
+            <td class="text-center">{{ application.Skills_Match_Percentage }}%</td>
+            <td class="text-center">
+                <!-- TODO: update this to show the skills of the applicant -->
+                <!-- <button @click="" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#editStaffModal">View skills</button> -->
+                <button @click="" class="btn btn-primary">View skills</button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
 
     <!-- Popup container -->
     <div class="popup-container" v-if="selectedApplication">
@@ -40,13 +81,15 @@
           <p class="card-text">Staff Name: {{ selectedApplication.Staff_Name }}</p>
           <p class="card-text">Department: {{ selectedApplication.Current_Dept }}</p>
           <p class="card-text">Skills Match Percentage: {{ selectedApplication.Skills_Match_Percentage }}%</p>
-          
+
           <button class="btn btn-primary" @click="hidePopup">Close</button>
         </div>
       </div>
     </div>
-    
   </div>
+    </div>
+
+    
 </template>
 
 <script>
@@ -57,7 +100,32 @@ export default {
     return {
       applications: [],
       selectedApplication: null,
+      selectedRoleNames: [],   // Array to store selected Role_Names
+      selectedDepts: [],
+      searchQuery: '', // Add this line to define searchQuery
     };
+  },
+
+  computed: {
+    filteredApplications() {
+      return this.applications.filter(app => {
+        const roleNameMatch = this.selectedRoleNames.length === 0 || this.selectedRoleNames.includes(app.Role_Name);
+        const deptMatch = this.selectedDepts.length === 0 || this.selectedDepts.includes(app.Current_Dept);
+        const searchMatch = !this.searchQuery ||
+          app.Application_ID.toString().includes(this.searchQuery) ||
+          app.Role_Name.includes(this.searchQuery) ||
+          app.Staff_Name.includes(this.searchQuery) ||
+          app.Current_Dept.includes(this.searchQuery);
+        return roleNameMatch && deptMatch && searchMatch;
+      });
+    },
+
+    uniqueRoleNames() {
+      return [...new Set(this.applications.map(app => app.Role_Name))];
+    },
+    uniqueDepts() {
+      return [...new Set(this.applications.map(app => app.Current_Dept))];
+    },
   },
 
   created() {
@@ -109,7 +177,7 @@ export default {
 }
 
 .table-container {
-  max-width: 800px;
+  max-width: 1000px;
   margin: 0 auto;
 }
 
@@ -160,5 +228,9 @@ export default {
 
 .popup button {
   padding: 5px 10px;
+}
+
+.filter-container{
+  padding-left: 30px;
 }
 </style>
