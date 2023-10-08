eeb64ad5d729a99afeb42a052ffe4d49184f1079
SEAN TAN <sean.tan.2021@scis.smu.edu.sg>
2023-10-08 17:33:55 +0800

    update skills filter

--- frontend/src/components/SkillsFilter.vue
+++ frontend/src/components/SkillsFilter.vue
@@ -1,4 +1,4 @@
-<template>
+<!-- <template>
   <div class="skills-filter">
     <h2>Skills Filter</h2>
     <div v-for="skill in uniqueSkills" :key="skill">
@@ -40,4 +40,46 @@ export default {
   },
 };
 </script>
+   -->
+
+   <template>
+    <div class="skills-filter">
+      <h2>Skills Filter</h2>
+      <select v-model="selectedSkill" @change="filterListings">
+        <option value="">Select a skill</option>
+        <option v-for="skill in uniqueSkills" :key="skill" :value="skill">{{ skill }}</option>
+      </select>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    props: {
+      listings: Array, // Array of listings with 'Skill_Name' attribute
+    },
+    data() {
+      return {
+        selectedSkill: '', // Selected skill
+      };
+    },
+    computed: {
+      uniqueSkills() {
+        // Get a unique list of skills from listings
+        const skillsSet = new Set();
+        this.listings.forEach(listing => {
+          if (listing.Skill_Name) {
+            skillsSet.add(listing.Skill_Name);
+          }
+        });
+        return Array.from(skillsSet);
+      },
+    },
+    methods: {
+      filterListings() {
+        // Emit the selected skill to the parent component
+        this.$emit('filter', this.selectedSkill);
+      },
+    },
+  };
+  </script>
   
