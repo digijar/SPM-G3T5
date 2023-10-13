444d42c5f1958bb17c24c700f2ccbc0c27851d06
ADAM TENG <adam.teng.2021@scis.smu.edu.sg>
2023-10-13 20:03:19 +0800

    Enhanced 'More Details' functionality with bootstrap modal

--- frontend/src/components/RoleDetailModal.vue
+++ frontend/src/components/RoleDetailModal.vue
@@ -1,12 +1,22 @@
 <template>
     <div class="modal" v-if="showModal">
-      <div class="modal-content">
-        <h2>{{ roleName }}</h2>
-        <p>{{ roleDescription }}</p>
-        <h3>{{ skillName }}</h3>
-        <p>{{ skillDescription }}</p>
-        <button @click="closeModal">Close</button>
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title">Details</h5>
+            <button @click="closeModal" type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+          </div>
+          <div class="modal-body">
+            <h3>Role Details: {{ roleName }}</h3>
+            <p>{{ roleDescription }}</p>
+            <h3>Skill Details: {{ skillName }}</h3>
+            <p>{{ skillDescription }}</p>
+          </div>
+          <div class="modal-footer">
+            <button @click="closeModal" type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+          </div>
       </div>
+    </div>
     </div>
   </template>
   
