790055b6a2c9fcec58d79176942e20eaef2868ea
SEAN TAN <sean.tan.2021@scis.smu.edu.sg>
2023-10-26 00:40:20 +0800

    Updated HR frontend - editjobmodal

--- frontend/src/components/EditJobModal.vue
+++ frontend/src/components/EditJobModal.vue
@@ -9,6 +9,11 @@
         <div class="modal-body">
           <form @submit.prevent="submitForm">
             <div class="form-floating mb-3">
+              <input type="text" class="form-control" id="InputValue_RoleName" v-model="roleName">
+              <label for="InputValue_RoleName">Role Name</label>
+            </div>
+
+            <div class="form-floating mb-3">
               <textarea class="form-control" id="InputValue_RoleDesc" v-model="roleDesc"></textarea>
               <label for="InputValue_RoleDesc">Role Description</label>
             </div>
@@ -46,6 +51,7 @@ export default {
 
   data() {
     return {
+      roleName: '',
       roleDesc: '',
       skillReq: '',
       deadline: '',
@@ -59,25 +65,26 @@ export default {
     },
 
     async fetchRoleSkillData() {
-    try {
-      const response = await axios.get('http://localhost:8080/get_roleskill_data');
-      const roleSkillData = response.data;
-      
-      const uniqueSkills = new Set();
-      this.skills = roleSkillData.filter((roleSkill) => {
-        if (!uniqueSkills.has(roleSkill.Skill_Name)) {
-          uniqueSkills.add(roleSkill.Skill_Name);
-          return true;
-        }
-        return false;
-      });
-    } catch (error) {
-      console.error(error);
-    }
-  },
+      try {
+        const response = await axios.get('http://localhost:8080/get_roleskill_data');
+        const roleSkillData = response.data;
+
+        const uniqueSkills = new Set();
+        this.skills = roleSkillData.filter((roleSkill) => {
+          if (!uniqueSkills.has(roleSkill.Skill_Name)) {
+            uniqueSkills.add(roleSkill.Skill_Name);
+            return true;
+          }
+          return false;
+        });
+      } catch (error) {
+        console.error(error);
+      }
+    },
 
     submitForm() {
       const data = {
+        roleName: this.roleName,
         roleDesc: this.roleDesc,
         skillReq: this.skillReq,
         deadline: this.deadline,
