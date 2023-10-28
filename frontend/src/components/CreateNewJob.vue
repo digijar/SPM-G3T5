29da9f1a434ff55d93d1b4eeeaec376a929798de
SEAN TAN <sean.tan.2021@scis.smu.edu.sg>
2023-10-28 17:08:23 +0800

    update createnewjob frontend

--- frontend/src/components/CreateNewJob.vue
+++ frontend/src/components/CreateNewJob.vue
@@ -11,14 +11,31 @@
         <div class="form-group">
           <div class="input-container">
             <label for="roleName" class="label">Role Name:</label>
-            <input type="text" id="roleName" v-model="newJob.roleName" class="input" required>
+            <input type="text" id="roleName" v-model="newJob.roleName" class="form-control" required>
+          </div>
+        </div>
+
+        <div class="form-group">
+          <div class="input-container">
+            <label for="RoleDesc" class="label">Role Description:</label>
+            <textarea class="form-control" id="RoleDesc" v-model="roleDesc"></textarea>
           </div>
         </div>
 
         <div class="form-group">
           <div class="input-container">
             <label for="skillRequired" class="label">Skill Required:</label>
-            <input type="text" id="skillRequired" v-model="newJob.skillRequired" class="input" required>
+            <select class="form-select" id="skillRequired" v-model="newJob.skillRequired">
+              <option value="" disabled>Select</option>
+              <option v-for="skill in skills" :key="skill.Skill_Name" :value="skill.Skill_Name">{{ skill.Skill_Name }}</option>
+            </select>
+          </div>
+        </div>
+
+        <div class="form-group">
+          <div class="input-container">
+            <label for="Deadline" class="label">Deadline:</label>
+            <input type="date" class="form-control" id="Deadline" v-model="deadline">
           </div>
         </div>
 
@@ -38,9 +55,23 @@ export default {
         roleName: '',
         skillRequired: '',
       },
+      skills: [], // Add a new data property for skills
     };
   },
+  created() {
+    // Fetch skill data when the component is created
+    this.fetchSkills();
+  },
   methods: {
+    fetchSkills() {
+      axios.get('http://localhost:8000/get_skill_data')
+        .then(response => {
+          this.skills = response.data;
+        })
+        .catch(error => {
+          console.error('Error fetching skill data:', error);
+        });
+    },
     submitJobListing() {
       // Prepare the data for submission
       const formData = {
@@ -72,7 +103,7 @@ export default {
   justify-content: space-between;
   align-items: center;
   margin-bottom: 20px;
-  padding: 10px; 
+  padding: 10px;
 }
 
 .page-heading {
@@ -82,39 +113,38 @@ export default {
 }
 
 .form-container {
-  text-align: center; 
-  margin-top: 16px; 
+  text-align: center;
+  margin-top: 16px;
 }
 
 .form-group {
   margin-bottom: 16px;
-  text-align: left; 
+  text-align: left;
 }
 
 .label {
-  font-size: 18px; 
-  font-weight: bold; 
+  font-size: 18px;
+  font-weight: bold;
 }
 
 .input-container {
-  width: 50%; 
-  margin: 0 auto; 
-  padding: 10px; 
+  width: 50%;
+  margin: 0 auto;
+  padding: 10px;
 }
 
 .input {
-  width: 100%; 
-  font-size: 18px; 
-  padding: 8px; 
+  width: 100%;
+  font-size: 18px;
+  padding: 8px;
 }
 
 .submit-button {
   font-size: 18px;
-  padding: 10px 20px; 
-  background-color: #007bff; 
-  color: #fff; 
+  padding: 10px 20px;
+  background-color: #007bff;
+  color: #fff;
   border: none;
   cursor: pointer;
 }
-
 </style>
